8d235f5a12ba8f0dbf381f75e8c13538a05ed1fb
Tokosama <anouartoko@gmail.com>
2025-03-31 11:58:39 +0100

    Implementation of the login functionnality

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -27,13 +27,16 @@ const userSchema = new mongoose.Schema({
 userSchema.pre("save", async function (next) {
 
   if(!this.isModified("password")) return next();
-  
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password,salt);
 
   next()
 });
 
+userSchema.methods.comparePassword = async function (userPassword){
+  return await bcrypt.compare(userPassword,this.password);
+}
 const User = mongoose.model("User", userSchema);
  
 export default User;
