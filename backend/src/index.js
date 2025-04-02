eb3aef70fbc681c95feb16d08f2e4f7f0428b01a
Tokosama <anouartoko@gmail.com>
2025-04-02 11:10:33 +0100

    fix of the cron bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,9 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
+app.get("/", (req, res) => {
+  res.json({test:"yo"});
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
