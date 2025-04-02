11c2a1c181a4a952a8fedc639766e1d48aabec78
Tokosama <anouartoko@gmail.com>
2025-04-02 10:08:48 +0100

    cron.js added to make the server alive periodically

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,21 @@
 import express from "express";
-import cors from "cors"
-import  "dotenv/config";
-import authRoutes from "./routes/authRoutes.js"
-import bookRoutes from "./routes/bookRoutes.js"
+import cors from "cors";
+import "dotenv/config";
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
-
+import job from "./lib/cron.js";
 
 const app = express();
-app.use(express.json())
-app.use(cors())
 const PORT = process.env.PORT || 3000;
-app.use("/api/auth",authRoutes);
-app.use("/api/books",bookRoutes);
 
+job.start();
+app.use(express.json());
+app.use(cors());
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  connectDB ()
+  connectDB();
 });
