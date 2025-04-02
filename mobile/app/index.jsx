f9c29fa0b82b0db775e04e786f8d232fb6860d83
Tokosama <anouartoko@gmail.com>
2025-04-02 12:10:24 +0100

    Login logic implementation

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,13 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
-import {Image} from "expo-image";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image } from "expo-image";
 import { Link } from "expo-router";
+
+import { useAuthStore } from "../store/authStore";
+import { useEffect } from "react";
 export default function Index() {
+  const { user, token, checkAuth, logout } = useAuthStore();
+  console.log(user, token);
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
+      <Text style={styles.title}>Hello {user?.username}</Text>
+      <TouchableOpacity onPress={logout}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
       <Link href="/(auth)/signup">Signup Page</Link>
       <Link href="/(auth)">LoginPage</Link>
-
     </View>
   );
 }
@@ -18,5 +29,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  title:{color:"red"}
+  title: { color: "red" },
 });
